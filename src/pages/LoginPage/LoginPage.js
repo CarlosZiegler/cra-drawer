2c690b784844fb94f79a0bb1c204f4486ad4b165
carlosziegler <carlos.ziegler@gmail.com>
2021-05-25 22:57:31 +0200

    add react query and delete login page

--- src/pages/LoginPage/LoginPage.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import * as S from './styles';
-
-export default function LoginPage() {
-  return <S.Container>LoginPage</S.Container>;
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,2 @@
-export { default as LoginPage } from './LoginPage/LoginPage';
 export { default as HomePage } from './HomePage/HomePage';
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
