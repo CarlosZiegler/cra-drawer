0d87e7404bbcea4f30a76a77280c108bad938e2e
carlosziegler <carlos.ziegler@gmail.com>
2021-05-27 00:01:31 +0200

    add basic skeleton

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styles';
 import { CompanyContext } from '../../context';
 import { useContext } from 'react';
+import { Skeleton } from 'antd';
 
 export default function HomePage() {
   const { company } = useContext(CompanyContext);
-  return <S.Container>{company?.name}</S.Container>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, [company]);
+
+  return (
+    <S.Container>
+      {isLoading ? <Skeleton active paragraph={{ rows: 8 }} /> : company?.name}
+    </S.Container>
+  );
 }
