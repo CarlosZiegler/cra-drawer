eaa299cca99e2a8ce7b28ed984ae5efe1732f1bd
carlosziegler <carlos.ziegler@gmail.com>
2021-05-27 00:27:32 +0200

    add context of workaround

--- src/tests/matchMedia.mock.js
+++ src/tests/matchMedia.mock.js
@@ -1,3 +1,8 @@
+// Workaround to solve TypeError: window.matchMedia is not a function is mentioned in Jest Documentation:
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+// but solved with other way here
+// https://stackoverflow.com/questions/64813447/cannot-read-property-addlistener-of-undefined-react-testing-library
+
 delete window.matchMedia;
 window.matchMedia = (query) => ({
   matches: false,
