dab0f954ff74cb91d74d9a28d0a00ed627f07882
carlosziegler <carlos.ziegler@gmail.com>
2021-05-27 18:34:05 +0200

    move form to other component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Sidebar } from './Sidebar';
 export { default as Alert } from './Alert';
 export { default as Drawer } from './Drawer';
+export { default as Form } from './Form';
--- src/pages/__tests__/homepage.test.js
+++ src/pages/__tests__/homepage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import HomePage from '../HomePage/HomePage';
 import MockContext from '../../tests/MockContext';
 import { CompanyBuilder } from '../../tests';
