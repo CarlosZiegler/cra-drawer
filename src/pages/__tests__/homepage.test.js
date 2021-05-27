6d7cfaa2ada6768222b6cc0d51ec7ea8a2d15504
carlosziegler <carlos.ziegler@gmail.com>
2021-05-27 18:20:36 +0200

    add test to show drawer

--- src/pages/__tests__/homepage.test.js
+++ src/pages/__tests__/homepage.test.js
@@ -1,13 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import HomePage from '../HomePage/HomePage';
 import MockContext from '../../tests/MockContext';
 import { CompanyBuilder } from '../../tests';
+import userEvent from '@testing-library/user-event';
 
 describe(HomePage, () => {
   it('Show Company from context', async () => {
     const { findByText } = await renderPage();
     const homeTitle = await findByText(mockCompany.name);
     expect(homeTitle).toBeInTheDocument();
+  });
+  it('Show drawer if clicked in edit button', async () => {
+    const { findByText } = await renderPage();
+    const editBtn = await findByText(/edit company/i);
+    userEvent.click(editBtn);
+
+    expect(await findByText(/create a new account/i)).toBeInTheDocument();
   });
 });
 
